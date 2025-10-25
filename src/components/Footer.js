1d5a48371459f8c744e8a36c9eff1c5b7c91c16d
ahotchkin <33204849+ahotchkin@users.noreply.github.com>
2025-10-25 08:31:24 -0400

    Add source code link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaCode } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 
 import '../styles/footer.css';
@@ -12,14 +12,6 @@ function Footer() {
         </p>
         <div className="footer-links">
           <a 
-            href="https://github.com/ahotchkin" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            aria-label="Visit my GitHub profile"
-          >
-            <FaGithub size={25} />
-          </a>
-          <a 
             href="https://www.linkedin.com/in/allyrh/" 
             target="_blank" 
             rel="noopener noreferrer"
@@ -27,11 +19,27 @@ function Footer() {
           >
             <FaLinkedin size={25} />
           </a>
+          <a 
+            href="https://github.com/ahotchkin" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Visit my GitHub profile"
+          >
+            <FaGithub size={25} />
+          </a>
           <a
             href="mailto:contact@allysonhotchkin.com"
             aria-label="Send me an email"
           >
             <MdEmail size={25} />
+          </a>
+          <a 
+            href="https://github.com/ahotchkin/ahotchkin.github.io/"
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="View the source code for this portfolio site"
+          >
+            <FaCode size={25} /> 
           </a>
         </div>
       </div>
